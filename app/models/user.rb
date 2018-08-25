906747372f66b4846753ca2a2dabcf93ed13cdc0
vagrant <vagrant@localhost.localdomain>
2018-08-26 01:05:27 +0900

    user.rbを更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :images
+  has_many :likes ,dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key:"follower_id",
+            dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+            foreign_key:"followed_id"
+            dependent: :destroy
+  has_many :following,through: :active_relationships,sourced: :followed
+  has_many :follower,through: :passive_relationships,sourced: :follower
 end
